b4a1e8dd1b08736b6e9aabc5f6d819e1b3eb8350
Edouard BONLIEU <ebonlieu@gmail.com>
2020-09-03 11:58:16 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+---
+display_name: Image resize
+status: ACTIVE
+version: 1.0.0
+---
+
+## Summary
+
+Image resize provides a simple way to resize and convert all your images into jpeg, png, or bmp format.
+
+## Advantages of using Koyeb's image resize Function
+
+With Koyeb, you can run the image resize integration to transform your images in seconds:
+
+- Automatically trigger the processing from any cloud: AWS S3, DigitalOcean Spaces, Google Cloud Storage, etc.
+- Store the result on the cloud service provider of your choice
+- Ready to use, no code or complex configuration required to get started
+- Combine additional processing actions that immediately use the results of the image resize function
+
+## Popular Use Cases with the image resize Function
+
+- Generate thumbnails
+- Convert images in different formats
+- Generate different version of your images
+
+## How the Imagga image Function Works with Koyeb
+
+To get started with the image resize function, you have to copy the configuration snippet below and replace the required values with yours.
+This Function is triggered each time an object is created in the Store you use.
+
+Below is what you need to use this Function:
+
+* A Koyeb valid account
+* A Store to upload images and save the function result
---
+++ image-resize.js
@@ -0,0 +1,105 @@
+/** @format */
+
+"use strict";
+const S3 = require("aws-sdk/clients/s3");
+const Jimp = require("jimp");
+
+const resizedImageFormats = {
+  jpeg: Jimp.MIME_JPEG,
+  png: Jimp.MIME_PNG,
+  bmp: Jimp.MIME_BMP,
+};
+
+const prefix = process.env.IMAGE_RESIZE_PATH || "resized-images/";
+
+const imageResize = async (s3Instance, bucket, key, watermarkImageUrl) => {
+  try {
+    let width = process.env.IMAGE_RESIZE_WIDTH
+      ? parseInt(process.env.IMAGE_RESIZE_WIDTH)
+      : Jimp.AUTO;
+    let height = process.env.IMAGE_RESIZE_HEIGHT
+      ? parseInt(process.env.IMAGE_RESIZE_HEIGHT)
+      : Jimp.AUTO;
+
+    const resizedImageFormat = process.env.IMAGE_RESIZE_FORMAT || "jpeg";
+
+    if (!(resizedImageFormat in resizedImageFormats)) {
+      throw new Error("Unsupported MIME type.");
+    }
+
+    const response = await s3Instance
+      .getObject({
+        Bucket: bucket,
+        Key: key,
+      })
+      .promise();
+
+    const image = await Jimp.read(response.Body);
+
+    image.resize(width, height);
+
+    const buffer = await image.getBufferAsync(
+      resizedImageFormats[resizedImageFormat]
+    );
+
+    await s3Instance
+      .upload({
+        Bucket: bucket,
+        Key: `${prefix}${key}`,
+        Body: buffer,
+        ContentType: resizedImageFormats[resizedImageFormat],
+      })
+      .promise();
+  } catch (error) {
+    throw error;
+  }
+};
+
+const getS3Configuration = (sourceBucket) => {
+  return {
+    accessKeyId: process.env[`KOYEB_STORE_${sourceBucket}_ACCESS_KEY`],
+    secretAccessKey: process.env[`KOYEB_STORE_${sourceBucket}_SECRET_KEY`],
+    region: process.env[`KOYEB_STORE_${sourceBucket}_REGION`],
+    endpoint: process.env[`KOYEB_STORE_${sourceBucket}_ENDPOINT`],
+  };
+};
+
+const validateEnvironment = (sourceBucket) => {
+  if (!sourceBucket) {
+    throw new Error("Bucket name not present in event payload.");
+  }
+
+  if (
+    !process.env?.[`KOYEB_STORE_${sourceBucket}_ACCESS_KEY`] ||
+    !process.env?.[`KOYEB_STORE_${sourceBucket}_SECRET_KEY`] ||
+    !process.env?.[`KOYEB_STORE_${sourceBucket}_REGION`] ||
+    !process.env?.[`KOYEB_STORE_${sourceBucket}_ENDPOINT`]
+  ) {
+    throw new Error(
+      `One of the following environment variables are missing: KOYEB_STORE_${sourceBucket}_ACCESS_KEY, KOYEB_STORE_${sourceBucket}_SECRET_KEY, KOYEB_STORE_${sourceBucket}_ENDPOINT, KOYEB_STORE_${sourceBucket}_REGION.`
+    );
+  }
+
+  if (!process.env?.IMAGE_RESIZE_WIDTH && !process.env?.IMAGE_RESIZE_HEIGHT) {
+    throw new Error(
+      `One of the following environment variables are missing: IMAGE_RESIZE_WIDTH, IMAGE_RESIZE_HEIGHT.`
+    );
+  }
+};
+
+const handler = async (event) => {
+  const bucket = event?.bucket?.name;
+  const key = event?.object?.key;
+
+  if (key.startsWith(prefix)) {
+    return;
+  }
+
+  validateEnvironment(bucket);
+
+  const s3Instance = new S3(getS3Configuration(bucket));
+
+  await imageResize(s3Instance, bucket, key);
+};
+
+module.exports.handler = handler;
